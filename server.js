0572475f2a12be48e92740099cf741cf808ed5f2
Jeremy Southon <southon55@gmail.com>
2019-05-12 16:00:08 +1200

    Apollo client setup, and querying working on local

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 
-// const cors = require('cors');
+const cors = require('cors');
 const schema = require('./schema');
 // const path = require('path');
 
 const app = express();
 
 // Allow cross-origin
-// app.use(cors());
+app.use(cors());
 
 app.use(
   '/graphql',
